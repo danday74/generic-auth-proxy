66a1563234612dc35995985545f39aa17ebbb090
Dan Lewis <daniellewis777@gmail.com>
2016-09-08 05:17:03 +0100

    Logging protocol

--- middlewares/1-request-logger.js
+++ middlewares/1-request-logger.js
@@ -2,7 +2,7 @@ const config = require('../server.config');
 
 let middleware = (req, res, next) => {
   /* istanbul ignore next */
-  config.logging && console.log(req.method, req.url);
+  config.logging && console.log(req.protocol.toUpperCase(), req.method, req.url);
   return next();
 };
 
