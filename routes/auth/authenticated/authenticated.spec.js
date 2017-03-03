084f448d6711d69962d62ffe6262f8a7e137c022
Dan Lewis <daniel.d.lewis@baesystems.com>
2017-03-03 15:28:51 +0000

    Tidied up auth spec

--- routes/auth/authenticated/authenticated.spec.js
+++ routes/auth/authenticated/authenticated.spec.js
@@ -2,68 +2,73 @@ const Imp = require('../_classes/TestImports');
 
 describe('/authenticated', () => {
 
-  it('should respond 401 where an invalid JWT token is given', (done) => {
+  describe('Success', () => {
 
-    let nocker = Imp.nock
-      .post(/validate-user$/, Imp.VALID_CREDENTIALS)
-      .reply(200, Imp.user);
+    it('should respond 200 where user is authorised', (done) => {
 
-    Imp.agent
-      .post('/login')
-      .send(Imp.VALID_CREDENTIALS)
-      .expect(200, (err) => {
+      let nocker = Imp.nock
+        .post(/validate-user$/, Imp.VALID_CREDENTIALS)
+        .reply(200, Imp.user);
 
-        nocker.done();
-        if (err) done(err);
+      Imp.agent
+        .post('/login')
+        .send(Imp.VALID_CREDENTIALS)
+        .expect(200, (err) => {
 
-        let jwtBrokenCookie = Imp.cookie.serialize(Imp.cfg.jwt.cookieName, 'invalid');
+          nocker.done();
+          if (err) done(err);
 
-        Imp.agent
-          .get('/authenticated')
-          .set('Cookie', jwtBrokenCookie)
-          .expect(401, (err) => {
-            done(err);
-          });
+          Imp.agent
+            .get('/authenticated')
+            .expect(200, (err) => {
 
-      });
+              if (err) done(err);
+
+              Imp.agent
+                .post('/logout')
+                .expect(200, (err) => {
+                  done(err);
+                });
+            });
+        });
+    });
+
   });
 
-  it('should respond 200 where a valid JWT token is given', (done) => {
+  describe('Failure', () => {
 
-    let nocker = Imp.nock
-      .post(/validate-user$/, Imp.VALID_CREDENTIALS)
-      .reply(200, Imp.user);
+    it('should respond 401 where user is unauthorised', (done) => {
 
-    Imp.agent
-      .post('/login')
-      .send(Imp.VALID_CREDENTIALS)
-      .expect(200, (err) => {
+      Imp.agent
+        .get('/authenticated')
+        .expect(401, (err) => {
+          done(err);
+        });
+    });
 
-        nocker.done();
-        if (err) done(err);
+    it('should respond 401 where JWT token is invalid', (done) => {
 
-        Imp.agent
-          .get('/authenticated')
-          .expect(200, (err) => {
-            done(err);
-          });
+      let jwtInvalidCookie = Imp.cookie.serialize(Imp.cfg.jwt.cookieName, 'invalid');
 
-      });
-  });
+      let nocker = Imp.nock
+        .post(/validate-user$/, Imp.VALID_CREDENTIALS)
+        .reply(200, Imp.user);
 
-  it('should respond 401 where no JWT cookie exists', (done) => {
+      Imp.agent
+        .post('/login')
+        .send(Imp.VALID_CREDENTIALS)
+        .expect(200, (err) => {
 
-    Imp.agent
-      .post('/logout')
-      .expect(200, (err) => {
-        if (err) done(err);
+          nocker.done();
+          if (err) done(err);
 
-        Imp.agent
-          .get('/authenticated')
-          .expect(401, (err) => {
-            done(err);
-          });
-
-      });
+          Imp.agent
+            .get('/authenticated')
+            .set('Cookie', jwtInvalidCookie)
+            .expect(401, (err) => {
+              done(err);
+            });
+        });
+    });
   });
 });
