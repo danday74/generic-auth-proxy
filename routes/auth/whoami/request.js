a22853192382b846a842d9aee8997559d2d80246
Daniel Lewis <daniellewis777@gmail.com>
2017-03-07 04:27:06 +0000

    changed import order

--- routes/auth/whoami/request.js
+++ routes/auth/whoami/request.js
@@ -1,6 +1,6 @@
-const validator = require('./validator');
-const config = require(appRoot + '/authServer.config');
 const jwt = require('jsonwebtoken');
+const config = require(appRoot + '/authServer.config');
+const validator = require('./validator');
 
 let route = router => {
   router.route('/whoami')
