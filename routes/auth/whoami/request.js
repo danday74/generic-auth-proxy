d83135cbb20626d59bfa482dc29d1912662d4c92
Daniel Lewis <daniellewis777@gmail.com>
2017-03-07 04:24:07 +0000

    changed whoami implementation

--- routes/auth/whoami/request.js
+++ routes/auth/whoami/request.js
@@ -6,12 +6,19 @@ let route = router => {
   router.route('/whoami')
     .get(validator, (req, res) => {
 
+      // no need to handle errors since we know this token is valid
       let token = req.cookies[config.jwt.cookieName];
       jwt.verify(token, config.jwt.secret, (err, decoded) => {
-        let restrictedUser = {
-          username: decoded.username,
-          email: decoded.email
-        };
+
+        const properties = ['username', 'email'];
+
+        const restrictedUser = Object.keys(decoded)
+          .filter(key => properties.includes(key))
+          .reduce((obj, key) => {
+            obj[key] = decoded[key];
+            return obj;
+          }, {});
+
         return res.status(200).json(restrictedUser);
       });
     });
