730d478649ae9417de88c2b3286d04dedeecbf26
Daniel Lewis <daniellewis777@gmail.com>
2017-03-14 05:28:57 +0000

    Updated git hook

--- gitHookPrePush.sh
+++ gitHookPrePush.sh
@@ -1,17 +1,55 @@
 #!/bin/sh
 
+# Version 1.1.5
 # This hook will PREVENT a GIT push where ..
 #   * The build is passing AND a commit message starts with WIP
 #   * The build is failing AND a commit message does not start with WIP
 #   * The build is failing AND the branch is master or develop
+
+echo;
+echo -e "pre-push: Executing pre-push git hook";
+wip=false;
+
+remote="$1"
+url="$2"
+
+z40=0000000000000000000000000000000000000000
+
+while read local_ref local_sha remote_ref remote_sha
+do
+	if [ "$local_sha" = ${z40} ]
+	then
+		# Handle delete
+		:
+	else
+		if [ "$remote_sha" = ${z40} ]
+		then
+			# New branch, examine all commits
+			range="$local_sha"
+		else
+			# Update to existing branch, examine new commits
+			range="$remote_sha..$local_sha"
+		fi
+
+		# Check for WIP commit
+		commit=`git rev-list -n 1 --grep '^WIP' "$range"`
+		if [ -n "$commit" ]
+		then
+			# echo >&2 "Found WIP commit in $local_ref, not pushing"
+			# exit 1
+			echo >&2 "pre-push: Found WIP commit";
+		  wip=true;
+		fi
+	fi
+done
+
+
 
 RED='\033[0;31m';
 YELLOW='\033[0;33m';
 GREEN='\033[0;32m';
 NC='\033[0m'; # No color
 
-echo;
-echo -e "pre-push: Executing pre-push git hook";
 echo -e "pre-push: Before pushing lets try building ..";
 echo;
 
@@ -26,36 +64,13 @@ else
     echo -e "pre-push: Build failed";
 fi
 
-remote="$1";
-url="$2";
-z40=0000000000000000000000000000000000000000;
-wip=false;
+
+
 branch=`git symbolic-ref --short HEAD`;
 prevent=false;
-
-while read local_ref local_sha remote_ref remote_sha; do
-	if [ "$local_sha" = ${z40} ]; then
-		# Handle delete
-		:
-	else
-		if [ "$remote_sha" = ${z40} ]; then
-			# New branch, examine all commits
-			range="$local_sha";
-		else
-			# Update to existing branch, examine new commits
-			range="$remote_sha..$local_sha";
-		fi
-
-		# Check for WIP commit
-		commit=`git rev-list -n 1 --grep '^WIP' "$range"`;
-		if [ -n "$commit" ]; then
-			echo >&2 "pre-push: Found WIP commit";
-		    wip=true;
-		fi
-	fi
-done
-
 echo -e "pre-push: SUMMARY: build=${build}, WIP=${wip}, branch=${branch}";
+
+
 
 if [ "${build}" == "true" ] && [ "${wip}" == "true" ]; then
   echo -e "pre-push: ${RED}Push prevented .. The build is passing AND a commit message starts with WIP";
@@ -88,6 +103,8 @@ fi
 if [ "${prevent}" == "false" ] && [ "${wip}" == "false" ]; then
   echo -e "pre-push: ${GREEN}Push permitted${NC}";
 fi
+
+
 
 test "${prevent}" == "false";
 exitCode=$?;
